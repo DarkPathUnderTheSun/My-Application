776949061d4524891ed956d2e640e91aa921ccf2
Hiram <ScarmMBA>
2023-03-09 20:15:11 -0600

    Commit de lenguaje.... todas las etiquetas del lenguaje ya realizadas, solo queda que el boton funcione

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,23 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Resources
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import kotlinx.coroutines.selects.select
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var Switch1: Switch
 
         val btn: Button = findViewById(R.id.registrarse)
 
@@ -28,7 +34,31 @@ class MainActivity : AppCompatActivity() {
             startActivity(intentSesion)
         }
 
+        Switch1.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                actualizarResource("es")
+            }else{
+                actualizarResource("en")
+            }
+
+
+        }
+
 
             }
+
+    fun actualizarResource(idioma: String){
+    val recursos = resources
+    val displayMetrics = recursos.displayMetrics
+    val configuracion = resources.configuration
+        configuracion.setLocale(Locale(idioma))
+        recursos.updateConfiguration(configuracion, displayMetrics)
+        configuracion.locale = Locale(idioma)
+        recursos.updateConfiguration(configuracion, displayMetrics)
+
+    }
+
+
+
     }
 
